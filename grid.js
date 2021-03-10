eeba561533f674f9a0f2f0a54a4a5f6d7bcb9206
A.Protasevich <larsplotner@gmail.com>
2021-03-10 18:36:11 +0300

    finished

--- grid.js
+++ grid.js
@@ -1,25 +1,59 @@
 const mainObj = document.querySelector('main');
-const gridObj = document.querySelector('.grid');
+let gridObj = document.querySelector('.grid');
 
-let gridSide = 256;
-//create 16x16 grid;
+let gridSquare = 256;
 
-const changeClassObj = (event) =>{
-    event.target.classList.toggle('gridCellh')
-};
 // initial state
 // creating tab with gridSide X gridSide
-for (let i=0; i<gridSide; i++){
-    const gridCellObj = document.createElement('div');
-    gridCellObj.classList.add('gridCell');
-    gridCellObj.addEventListener('mouseover', changeClassObj);
-    gridObj.appendChild(gridCellObj);
-};
 
-const resetButtonObj = document.createElement('button');
+//reset func
+
+const resetFunction = (gridSide) => {
+    //get all grid cells
+    gridSide = Number(prompt('Enter size of one side (100 max)'))//**2;
+    gridSquare = gridSide**2;
+    gridObj = document.querySelector('.grid');
+    if (gridSide<21){
+
+        mainObj.removeChild(gridObj);
+        let _gridObj = document.createElement('div');
+        _gridObj.classList.add('grid');
+        mainObj.appendChild(_gridObj);
+        createGrid(gridSquare);
+    
+    }else{
+        alert('mistake!!! size of one side must be 100 max')        
+    }
+
+}
+
+const createGrid = (gridSquare, grid) => {
+    grid = document.querySelector('.grid');
+    let gridSide = gridSquare**(1/2);
+    grid.style.gridTemplateRows = `repeat(${gridSide}, 50px)`;
+    grid.style.gridTemplateColumns = `repeat(${gridSide}, 50px)`;
+    const changeClassObj = (event) =>{
+        event.target.classList.toggle('gridCellh')
+    };
+
+    for (let i=0; i<gridSquare; i++){
+        const gridCellObj = document.createElement('div');
+        gridCellObj.classList.add('gridCell');
+        gridCellObj.addEventListener('mouseover', changeClassObj);
+        grid.appendChild(gridCellObj);
+    };
+
+    const resetButtonObj = document.createElement('button');
+    resetButtonObj.textContent = 'RESET BUTTON'
+    resetButtonObj.style.height = `50px`;
+    resetButtonObj.style.width = getComputedStyle(grid)['width'];
+    resetButtonObj.addEventListener('click',resetFunction )
+    grid.appendChild(resetButtonObj);
+
+} 
+createGrid(gridSquare); //create 16x16 grid;
+
+
+
 
 // resetButtonObj.classList.add('resetButton');
-resetButtonObj.textContent = 'RESET BUTTON'
-resetButtonObj.style.height = `50px`;
-resetButtonObj.style.width = getComputedStyle(gridObj)['width'];
-gridObj.appendChild(resetButtonObj);
